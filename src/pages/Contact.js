cbcc1fc8dc4d0ad571d3f324ca24ff86c67d050b
nima <nimiologyy@gmail.com>
2024-10-26 14:07:47 +0330

    contact

---
+++ src/pages/Contact.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Home.css'; // Adjust the path if necessary
+
+const ContactUs = () => {
+    return (
+        <div className="main-content" style={{justifyContent: 'center'}}>
+            <div className="contact-container">
+                <h1>Contact Us</h1>
+                <p>For App Questions: <a href="mailto:support@hallery.art">support@hallery.art</a></p>
+                <p>For General Inquiries: <a href="mailto:info@hallery.art">info@hallery.art</a></p>
+                <p>For Privacy Concerns: <a href="mailto:privacy@hallery.art">privacy@hallery.art</a></p>
+                <p>For Feedback: <a href="mailto:feedback@hallery.art">feedback@hallery.art</a></p>
+            </div>
+        </div>
+    );
+}
+
+export default ContactUs;
