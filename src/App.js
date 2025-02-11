7eaa68bb282370ee21993b6a3b0a20fd1a9aed58
nima <nimiologyy@gmail.com>
2025-02-12 00:20:25 +0330

    paths ios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 import Home from "./pages/Home";
 import ForgetPassword from "./pages/ForgetPassword";
 import Contact from "./pages/Contact";
 import Success from "./pages/Success";
 import Profile from "./pages/Profile";
 import Artwork from "./pages/Artwork";
+import {Analytics} from "@vercel/analytics/react";
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/success" element={<Success />} />
-                <Route path="/reset-password/:uid/:token/" element={<ForgetPassword />} />
-                <Route path="/profile/:profileId/" element={<Profile />} />
-                <Route path="/artwork/:artworkId/" element={<Artwork />} />
-                <Route path="/contact" element={<Contact />} />
+                <Route path="/" element={<Home/>}/>
+                <Route path="/success" element={<Success/>}/>
+                <Route path="/reset-password/:uid/:token/" element={<ForgetPassword/>}/>
+                <Route path="/profile/:profileId/" element={<Profile/>}/>
+                <Route path="/artwork/:artworkId/" element={<Artwork/>}/>
+                <Route path="/contact" element={<Contact/>}/>
             </Routes>
+            <Analytics/>
+
         </Router>
+
     );
 }
 
